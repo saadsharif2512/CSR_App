b1b5d5eac290092c8cd80d4c412fd6ae3d9f2112
Aarij Khan <aarijomark@gmail.com>
2025-11-08 14:57:30 +0400

    made more changes to manifest

--- student_management/__manifest__.py
+++ student_management/__manifest__.py
@@ -10,9 +10,9 @@
     'depends': ['base', 'mail'],
     'data': [
         'security/ir.model.access.csv',
-        'views/student_views.xml',
-        'views/course_views.xml',
-        'views/enrollment_views.xml',
+        'views/hr_views.xml',
+        'views/res_company_views.xml',
+        'views/stock_views.xml',
         'views/menu.xml',
         'data/demo_data.xml',
     ],
